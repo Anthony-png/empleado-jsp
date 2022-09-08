d93c2ca468d81457010a16f15523a837c7cc3e16
CDE-LAB3-PC28 <CDE-LAB3-PC28@FERNANDOUNINORT>
2022-09-07 21:23:24 -0400

    configuraciones de dependencias

---
+++ src/main/java/com/cuenca/empleados/model/Empleado.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.cuenca.empleados.model;
+
+/**
+ *
+ * @author CDE-LAB3-PC28
+ */
+public class Empleado {
+    private int id;
+    
+    public int getId(){
+        return id;
+    }
+    public void setId(int ident){
+        this.id = ident;
+    }
+}
